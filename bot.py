b59fe0302bed21fad257bcf344fc54b766f1ed51
urik86 <83026908+urik86@users.noreply.github.com>
2021-04-29 12:00:35 +0200

    Creación de bot.py
    Creación del archivo principal del bot, con funciones de test todavía

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+
+from discord.ext import commands
+from discord_slash import SlashCommand # Importing the newly installed library.
+
+bot = commands.Bot(command_prefix='!LADA ' , case_insesitive=True)
+slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the client.
+
+@bot.event
+async def on_ready():
+    print("Ready!")
+
+guild_ids = [834195635574014043] # Put your server ID in this array.
+
+@slash.slash(name="ping", guild_ids=guild_ids)
+async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
+    await ctx.send(f"Pong! ({bot.latency*1000}ms)")
+
+bot.run(os.getenv('TOKEN'))
