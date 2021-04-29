09742f5a21fe0da75c5f005f70f21f0e047a8fb2
uriel miralles <>
2021-04-29 14:10:17 +0000

    Prueba inicial de comandos slash

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from discord.ext import commands
 from discord_slash import SlashCommand # Importing the newly installed library.
 
+
 bot = commands.Bot(command_prefix='!LADA ' , case_insesitive=True)
 slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the client.
 
@@ -10,10 +11,12 @@ slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through 
 async def on_ready():
     print("Ready!")
 
-guild_ids = [834195635574014043] # Put your server ID in this array.
+guild_ids = [555142549757493254] # Put your server ID in this array.
 
-@slash.slash(name="ping", guild_ids=guild_ids)
+@slash.slash(name="ping", description='Testear ping del bot', guild_ids=guild_ids)
 async def _ping(ctx): # Defines a new "context" (ctx) command called "ping."
     await ctx.send(f"Pong! ({bot.latency*1000}ms)")
+
+
 
 bot.run(os.getenv('TOKEN'))
