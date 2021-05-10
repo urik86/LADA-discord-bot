9b0aff9580b09197fbf0d1b6c148628b8356457e
Uriel Miralles <>
2021-05-10 18:09:48 +0000

    Actualización de canal de sistema

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import os
 import discord
 from discord.ext import commands
-from discord_slash import SlashCommand # Importing the newly installed library.
+from discord_slash import SlashCommand
 from bolsacaos.caos import draw_caos
 
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(intents = intents, command_prefix='!LADA' , case_insesitive=True)
-slash = SlashCommand(bot, sync_commands=True) # Declares slash commands through the client.
+slash = SlashCommand(bot, sync_commands=True)
 
 guild_ids = [555142549757493254,834195635574014043]
 
 @bot.event
 async def on_ready():
     print("Conectado correctamente con {0.user}".format(bot))
+    await bot.change_presence(activity=discord.Game('saludar por el general'))
 
 @bot.event
 async def on_member_join(member):
-  channel = bot.get_channel(834443569279664128)
+  channel = member.guild.system_channel
   colorsenior = 0x3e55e4
   colormayor = 0xc2182b
   colorjunior = 0x337731
@@ -42,7 +43,7 @@ async def on_member_join(member):
                   name="caos",
                   description='Roba una ficha de la bolsa del caos',
                   guild_ids=guild_ids)
-async def _caos(ctx): # Defines a new "context" (ctx) command called "ping."
+async def _caos(ctx):
   channel = bot.get_channel(ctx.channel_id)
   resultado = draw_caos (ctx)
   await ctx.send('Sacando un token de la bolsa del caos...')
